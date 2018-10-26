1f89cf039edc0a6bd047b1d8a5d52acc9ad64229
Rebeca Oprea <rebekaoprea@yahoo.com>
2018-10-26 18:06:12 +0300

    Implemented constructors and getters
    Added:
     global variables
     constructors
     getters methods
     toString methods

---
+++ BigInteger.cpp
@@ -0,0 +1,44 @@
+#include "pch.h"
+#include "BigInteger.h"
+
+
+BigInteger::BigInteger()
+{
+}
+
+BigInteger::BigInteger(const char* numberSequence)
+{
+	this->numberSequence = (char*)numberSequence;
+	this->base = 10;
+	this->isUnsigned = numberSequence[0] != '-';
+}
+
+BigInteger::BigInteger(const char* numberSequence, int base)
+{
+	this->numberSequence = (char*)numberSequence;
+	this->base = base;
+	this->isUnsigned = numberSequence[0] == '-';
+}
+
+BigInteger::~BigInteger()
+{
+	delete this->numberSequence;
+	delete &this->base;
+	delete &this->isUnsigned;
+}
+
+int BigInteger::getBase()
+{
+	return this->base;
+}
+
+
+char BigInteger::getSign()
+{
+	return this->isUnsigned ? '+' : '-';
+}
+
+char* BigInteger::toString()
+{
+	return this->numberSequence;
+}
---
+++ BigInteger.h
@@ -0,0 +1,19 @@
+#pragma once
+class BigInteger
+{
+private:
+	char* numberSequence;
+	int base;
+	bool isUnsigned;
+
+public:
+	BigInteger();
+	BigInteger(const char* numberSequence);
+	BigInteger(const char* numberSequence, int base);
+	virtual ~BigInteger();
+	int getBase();
+	char getSign();
+	char* toString();
+	
+};
+
