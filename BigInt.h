003ca03141201ff24bd7db09808a66c78ee060f8
Unknown <rebekaoprea@yahoo.com>
2018-10-29 23:44:33 +0200

    class update v.1.19

--- BigInt.h
+++ BigInt.h
@@ -129,6 +129,11 @@ bool operator < (BigInteger a, int b)
 	return (a < Integer(b));
 }
 
+bool operator > (BigInteger a, int b) 
+{
+	return (a > Integer(b));
+}
+
 
 
 BigInteger maxim(BigInteger a, BigInteger b)
