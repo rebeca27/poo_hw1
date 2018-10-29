37969270b83fba2e701c0f26850ed2ccf9d1e9c9
Rebeca Oprea <rebekaoprea@yahoo.com>
2018-10-29 18:06:26 +0200

    Class 1.0

---
+++ BigInt.h
@@ -0,0 +1,22 @@
+class BigInteger
+{
+private:
+	char* numberSequence;
+	int base;
+	bool isUnsigned;
+
+public:
+	BigInteger();
+	BigInteger(const char* numberSequence);
+	BigInteger(const char* numberSequence, int base);
+	virtual ~BigInteger();
+	int getBase();
+	char getSign();
+	char* toString();
+
+};
+
+
+
+
+
