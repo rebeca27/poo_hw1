bde45330352c2d2f59f6c905b2bed7934249016d
rebeca27 <38529211+rebeca27@users.noreply.github.com>
2018-11-05 14:44:20 +0200

    Update BigInt.h

--- BigInt.h
+++ BigInt.h
@@ -21,63 +21,6 @@ public:
 	char getSign();
 	char* toString();
 
-};
-
-const int baza = 10;
-
-BigInteger Integer(char a[])                  //returneaza forma string a unui numar
-{
-	string r = "";
-	for(i = 0; i <= strlen(a) - 1; i++) 
-		r = r + a[i];
-	return r;
-}
-
-void Set(BigInteger &bi)             //se repeta structura while de cate ori vectorul are mai mult de 1 elem,
-				     //dar referinta ultimului element din vector este primul	
-	
-{
-	while (bi.size() > 1 && bi.back() == 0) bi.pop_back();
-}
-
-BigInteger Integer(string s) 
-{
-	BigInteger lol;
-	if (s[0] == '-') return lol;				//daca nuamrul este negativ se intoarce
-	if (s.size() == 0) 
-	{ lol.push_back(0); 
-	return lol; 
-	}
-	while (s.size() % 9 != 0) s = '0' + s;
-	for (int i = 0; i < s.size(); i += 9) {
-		int v = 0;
-		for (int j = i; j < i + 9; j++) v = v * 10 + (s[j] - '0');
-		lol.insert(lol.begin(), v);
-	}
-	Set(lol);
-	return lol;
-}
-
-BigInteger Integer(char ch[]) 
-{
-	string s = "";
-	for(i = 0; i <= strlen(ch) - 1; s = s + ch[i]; i++)
-	return Integer(s);
-}
-
-BigInteger Integer(b x)
-{
-	string r = "";
-	while (x > 0)
-		r = char(x % 10 + '0') + r, x /= 10;
-	return Integer(r);
-}
-
-BigInteger Integer(int x)
-{
-	return Integer((b)x);
-}
-
 void operator ++() 
     { 
         count = count+1; 
@@ -104,282 +47,53 @@ void operator << (ostream &out, BigInteger a)
 	printf("\n");
 }
 
-bool operator < (BigInteger x, BigInteger y)
-{
-	Set(x);
-	Set(y);
-	if (x.size() != y.size())
-		return (x.size() < y.size());
-	for (int i = x.size() - 1, i >= 0; i--)
-		if (x[i] != y[i])
-			return (x[i] < y[i]);
-	return false;
-}
+friend bool operator < (BigInteger x, BigInteger y);
+friend bool operator > (BigInteger a, BigInteger b);
+friend bool operator == (BigInteger a, BigInteger b);
+friend bool operator <= (BigInteger a, BigInteger b);
+friend bool operator >= (BigInteger a, BigInteger b);
+friend bool operator < (BigInteger a, int b);
+friend bool operator > (BigInteger a, int b);
+friend bool operator == (BigInteger a, int b);
+friend bool operator >= (BigInteger a, int b);
+friend bool operator <= (BigInteger a, int b);
+friend BigInteger maxim(BigInteger a, BigInteger b);
+friend BigInteger minim(BigInteger a, BigInteger b);
+friend BigInteger operator + (BigInteger a, BigInteger b);
+friend BigInteger operator + (BigInteger a, int b);
+friend BigInteger operator ++ (BigInteger &a); 
 
-bool operator > (BigInteger a, BigInteger b) 
-{
-	return (b < a);
-}
+void operator += (BigInteger &a, BigInteger b);
+void operator += (BigInteger &a, int b);
+	
+friend BigInteger operator - (BigInteger a, BigInteger b) ;
+friend BigInteger operator - (BigInteger a, int b);
 
-bool operator == (BigInteger a, BigInteger b)
-{
-	return (!(a < b) && !(b < a));
-}
+friend void operator -- (BigInteger &a);
+void operator -= (BigInteger &a, BigInteger b);
+void operator -= (BigInteger &a, int b);
 
-bool operator <= (BigInteger a, BigInteger b) 
-{
-	return (a < b || a == b);
-}
+friend BigInteger operator * (BigInteger t, BigInteger b);
 
-bool operator >= (BigInteger a, BigInteger b)
-{
-	return (b < a || b == a);
-}
+friend BigInteger operator * (BigInteger a, int b);
 
-bool operator < (BigInteger a, int b) 
-{
-	return (a < Integer(b));
-}
+void operator *= (BigInteger &a, BigInt b) ;
 
-bool operator > (BigInteger a, int b) 
-{
-	return (a > Integer(b));
-}
+void operator *= (BigInteger &a, int b) ;
 
-bool operator == (BigInteger a, int b)
-{
-	return (a == Integer(b));
-}
+friend BigInteger operator / (BigInteger a, BigInteger b) ;
 
-bool operator >= (BigInteger a, int b)
-{
-	return (a >= Integer(b));
-}
+friend BigInteger operator / (BigInteger t, int b) ;
 
-bool operator <= (BigInteger a, int b)
-{
-	return (a <= Integer(b));
-}
+void operator /= (BigInteger &a, BigInteger b) ;
 
-BigInteger maxim(BigInteger a, BigInteger b)
-{
-	if (a > b) return a;
-	return b;
-}
+void operator /= (BigInteger &a, int b) ;
 
-BigInteger minim(BigInteger a, BigInteger b) 
-{
-	if (a < b) return a;
-	return b;
-}
+friend BigInteger operator % (BigInteger a, BigInteger b);
 
-BigInteger operator + (BigInteger a, BigInteger b)
-{
-	Set(a);
-	Set(b);
-	BigInt lol;
-	int rest = 0;
-	int j = max(a.size(), b.size());
- 	for(int i = 0; i <= j - 1; i++) 
-	{
-		if (i < a.size()) rest += a[i];
-		if (i < b.size()) rest += b[i];
-		lol.push_back(rest%baza);
-		rest /= baza;
-	}
-	if (rest) 
-		bas.push_back(rest);
-	Set(lol);
-	return lol;
-}
+friend int operator % (BigInteger a, int b);
 
-BigInteger operator + (BigInteger a, int b)
-{
-	return a + Integer(b);
-}
-
-BigInteger operator ++ (BigInteger &a) 
-{ 
-	a = a + 1;
-	return a;
-}
-
-void operator += (BigInteger &a, BigInteger b)
-{
-	a = a + b;
-}
-
-void operator += (BigInteger &a, int b) 
-{
-	a = a + b;
-}
-
-BigInteger operator - (BigInteger a, BigInteger b) 
-{
-	Set(a);
-	Set(b);
-	BigInteger lol;
-	int rest = 0; 
-	for(i = 0; i <= a.size() - 1; i++)
-	{
-		rest += a[i] - (i < b.size() ? b[i] : 0);
-		if (rest < 0)
-			lol.push_back(rest + baza), rest = -1;
-		else lol.push_back(rest), rest = 0;
-	}
-	Set(lol);
-	return lol;
-}
-
-BigInteger operator - (BigInteger a, int b)
-{
-	return a - Integer(b);
-}
-
-void operator -- (BigInteger &a) 
-{ 
-	a = a - 1;
-}
-
-void operator -= (BigInteger &a, BigInteger b) 
-{
-	a = a + b;
-}
-
-void operator -= (BigInteger &a, int b) 
-{
-	a = a - b;
-}
-
-
-BigInteger operator * (BigInteger t, BigInteger b)
-{
-	Set(t);
-	Set(b);
-	BigInteger lol;
-	lol.assign(t.size() + b.size(), 0);
-	for (i = 0; i <= t.size() - 1; i++)
-	{
-		a carry = 0ll;
-		for (int j = 0; j < b.size() || rest > 0; j++)
-		{
-			a s = lol[i + j] + rest + (a)t[i] * (j < b.size() ? (a)b[j] : 0ll);
-			lol[i + j] = s % baza;
-			rest = s / baza;
-		}
-	}
-	Set(lol);
-	return lol;
-}
-
-BigInteger operator * (BigInteger a, int b)
-{
-	return a * Integer(b);
-}
-
-void operator *= (BigInteger &a, BigInt b) 
-{
-	a = a * b;
-}
-
-void operator *= (BigInteger &a, int b) 
-{
-	a = a * b;
-}
-
-
-BigInteger operator / (BigInteger a, BigInteger b) 
-{
-	Set(a);
-	Set(b);
-	if (b == Integer(0)) return Integer("-1");
-	BigInteger lol, ent;
-
-	for (int i = a.size() - 1; i >= 0; i--)
-	{
-		cur.insert(cur.begin(), a[i]);
-		int x = 0, L = 0, R = baza;
-		while (L <= R) {
-			int mid = (L + R) >> 1;
-			if (b*Integer(mij) > ent)
-			{
-				x = mij;
-				R = mij - 1;
-			}
-			else
-				L = mij + 1;
-		}
-		ent = ent - Integer(x - 1)*b;
-		ans.insert(ans.begin(), x - 1);
-	}
-	Set(lol);
-	return lol;
-}
-
-BigInteger operator / (BigInteger t, int b) 
-{
-	Set(t);
-	BigInteger lol;
-	t ent = 0ll;
-	for(i = t.size() - 1; i >= 0; i--)
-	{
-		ent = (ent*(a)baza + (a)t[i]);
-		lol.insert(lol.begin(), ent / b);
-		ent %= b;
-	}
-	Set(lol);
-	return lol;
-}
-
-void operator /= (BigInteger &a, BigInteger b) {
-	a = a / b;
-}
-
-void operator /= (BigInteger &a, int b) {
-	a = a / b;
-}
-
-
-BigInteger operator % (BigInteger a, BigInteger b)
-{
-	Set(a);
-	Set(b);
-	if (b == Integer(0)) return Integer("-1");
-	BigInteger lol;
-	for(i = a.size() - 1; i >= 0; i--)
-	{
-		lol.insert(lol.begin(), a[i]);
-		int x = 0, L = 0, R = baza;
-		while (L <= R) {
-			int mij = (L + R) >> 1;
-			if (b*Integer(mij) > lol) {
-				x = mij;
-				R = mij - 1;
-			}
-			else
-				L = mij + 1;
-		}
-		lol = lol - Integer(x - 1)*b;
-	}
-	Set(lol);
-	return lol;
-}
-
-int operator % (BigInteger a, int b)
-{
-	Set(a);
-	if (b == 0) return -1;
-	int gcc = 0;
-	for (i = a.size() - 1; i >= 0; i--)
-		gcc = (gcc*(base%b) + a[i] % b) % b;
-	return gcc;
-}
-
-void operator %= (BigInteger &a, BigInteger b) {
-	a = a % b;
-}
-
-void operator %= (BigInteger &a, int b) {
-	a = a % Integer(b);
-}
-
+void operator %= (BigInteger &a, BigInteger b);
+void operator %= (BigInteger &a, int b);
+	
+;}
